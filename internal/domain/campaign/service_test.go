b58c648048b3c5d43b82dce9f8ea129b7cfd1afb
Rubens Rabelo <rubensrabelo123@gmail.com>
2026-04-24 12:56:13 -0300

    add endpoints

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -19,78 +19,65 @@ func (r *repositoryMock) Save(campaign *Campaign) error {
 	return args.Error(0)
 }
 
+func (r *repositoryMock) Get() ([]Campaign, error) {
+	//args := r.Called(campaign)
+	return nil, nil
+}
+
 var (
 	newCampaign = contract.NewCampaign{
 		Name:    "Test Y",
-		Content: "Body Hi",
+		Content: "Body Hi!",
 		Emails:  []string{"teste1@test.com"},
 	}
+	service = Service{}
 )
 
 func Test_Create_Campaign(t *testing.T) {
 	assert := assert.New(t)
-
-	repo := new(repositoryMock)
-	repo.On("Save", mock.Anything).Return(nil)
-	service := Service{Repository: repo}
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("Save", mock.Anything).Return(nil)
+	service.Repository = repositoryMock
 
 	id, err := service.Create(newCampaign)
 
+	assert.NotNil(id)
 	assert.Nil(err)
-	assert.NotEmpty(id)
-
-	repo.AssertExpectations(t)
 }
 
 func Test_Create_ValidateDomainError(t *testing.T) {
 	assert := assert.New(t)
 
-	campaignInvalid := newCampaign
-	campaignInvalid.Name = ""
+	_, err := service.Create(contract.NewCampaign{})
 
-	repo := new(repositoryMock)
-	service := Service{Repository: repo}
-
-	_, err := service.Create(campaignInvalid)
-
-	assert.NotNil(err)
-	assert.Contains(err.Error(), "name") 
 	assert.False(errors.Is(internalerrors.ErrInternal, err))
 }
 
 func Test_Create_SaveCampaign(t *testing.T) {
-	assert := assert.New(t)
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
+		if campaign.Name != newCampaign.Name ||
+			campaign.Content != newCampaign.Content ||
+			len(campaign.Contacts) != len(newCampaign.Emails) {
+			return false
+		}
 
-	repo := new(repositoryMock)
-	repo.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
-		return campaign.Name == newCampaign.Name &&
-			campaign.Content == newCampaign.Content &&
-			len(campaign.Contacts) == len(newCampaign.Emails)
+		return true
 	})).Return(nil)
+	service.Repository = repositoryMock
 
-	service := Service{Repository: repo}
+	service.Create(newCampaign)
 
-	id, err := service.Create(newCampaign)
-
-	assert.Nil(err)
-	assert.NotEmpty(id)
-
-	repo.AssertExpectations(t)
+	repositoryMock.AssertExpectations(t)
 }
 
 func Test_Create_ValidateRepositorySave(t *testing.T) {
 	assert := assert.New(t)
-
-	repo := new(repositoryMock)
-	repo.On("Save", mock.Anything).Return(errors.New("Internal Server Error"))
-
-	service := Service{Repository: repo}
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("Save", mock.Anything).Return(errors.New("error to save on database"))
+	service.Repository = repositoryMock
 
 	_, err := service.Create(newCampaign)
 
-	assert.NotNil(err)
-	assert.Equal("Internal Server Error", err.Error())
-
-	repo.AssertExpectations(t)
-	assert.True(errors.Is(err, internalerrors.ErrInternal))
+	assert.True(errors.Is(internalerrors.ErrInternal, err))
 }
